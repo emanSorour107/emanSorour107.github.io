2331dc57519fe776db4f510ed67c6698d4361289
emanSorour107 <esorour@miu.edu>
2020-06-02 11:06:54 -0500

    b

--- lab7/account.js
+++ lab7/account.js
@@ -1,26 +1,39 @@
-let accountInfoList =[];
 
-let oneAccount = (name, balance) => {
-return {name, balance};
-};
+(function () {
+    "use strict";
+
+    let accountInfoList = [];
+
+    const accountfn = (function() {
+       
+        return{
+        oneAccount: (name, balance) => {
+            return { name, balance };
+        }
+    }
+    })();
 
 
-function addAccount(){
-    let account = document.getElementById("accountName");
-    let deposite = document.getElementById("deposit");
-    accountInfoList.push(oneAccount(account.value, deposite.value));
-    account.value ='';
-    deposite.value ='';
-    displayResult();
-    
-}
 
-var displayResult = function(){
-    let result = document.getElementById("textarea");
-    let totalResult ='';
-for (let i = 0; i < accountInfoList.length; i++) {
-    totalResult += `Account name: ${accountInfoList[i].name}    Balance: ${accountInfoList[i].balance} \n`;
-};
-result.innerHTML = totalResult;
-}
+    window.onload = (function () {
+        let butnClick = document.getElementById("createAccount");
+        butnClick.onclick = (function () {
+            let account = document.getElementById("accountName");
+            let deposite = document.getElementById("deposit");
+            accountInfoList.push(accountfn.oneAccount(account.value, deposite.value));
+            account.value = '';
+            deposite.value = '';
+            displayResult();
+        })
+    })
 
+    var displayResult = function () {
+        let result = document.getElementById("textarea");
+        let totalResult = '';
+        for (let i = 0; i < accountInfoList.length; i++) {
+            totalResult += `Account name: ${accountInfoList[i].name}    Balance: ${accountInfoList[i].balance} \n`;
+        };
+        result.innerHTML = totalResult;
+    }
+
+})();
